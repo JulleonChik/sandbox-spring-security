c60bd386664e43010609c0443d0cc54922f6cf91
Julleon <julleonchik@gmail.com>
2024-02-23 03:04:19 +0900

    Five ways to access information about an authenticated user

--- src/main/java/pro/julleon/sandbox/SandboxSpringSecurityApplication.java
+++ src/main/java/pro/julleon/sandbox/SandboxSpringSecurityApplication.java
@@ -2,12 +2,53 @@ package pro.julleon.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.function.*;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 
 @SpringBootApplication
 public class SandboxSpringSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SandboxSpringSecurityApplication.class, args);
+    }
+
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .httpBasic(Customizer.withDefaults())
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().authenticated()
+//                );
+//        return http.build();
+//    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+        return RouterFunctions.route()
+                .GET("/api/v4/greetings", request -> {
+                    UserDetails userDetails = request.principal()
+                            .map(Authentication.class::cast)
+                            .map(Authentication::getPrincipal)
+                            .map(UserDetails.class::cast)
+                            .orElseThrow();
+
+                    return ServerResponse.ok()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(Map.of("greeting", "Hello %s!".formatted(userDetails.getUsername())));
+                })
+                .build();
     }
 
 }
