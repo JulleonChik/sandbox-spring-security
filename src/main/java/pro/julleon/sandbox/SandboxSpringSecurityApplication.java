60eb903c9699a9533bad73f8a9b98ef86f539f76
Julleon <julleonchik@gmail.com>
2024-02-23 23:01:55 +0900

    В этой реализации используется явно созданный объект BasicAuthenticationEntryPoint и устанавливается его имя области (RealmName). В предыдущей реализации использовалась стандартная точка входа в систему, предоставляемая Spring Security. В обоих случаях настраивается базовая аутентификация и устанавливается точка входа в систему для обработки ситуаций без аутентификации.

--- src/main/java/pro/julleon/sandbox/SandboxSpringSecurityApplication.java
+++ src/main/java/pro/julleon/sandbox/SandboxSpringSecurityApplication.java
@@ -1,5 +1,8 @@
 package pro.julleon.sandbox;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,13 +10,18 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.RouterFunctions;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.io.IOException;
 import java.util.Map;
 
 @SpringBootApplication
@@ -24,37 +32,81 @@ public class SandboxSpringSecurityApplication {
     }
 
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                // Конфигурация базовой аутентификации. В данном случае, закомментирована, возможно, не используется.
-                //.httpBasic(Customizer.withDefaults())
+    /*
+AuthenticationEntryPoint - это интерфейс в Spring Security, предназначенный для обработки ситуаций,
+когда пользователь не прошел аутентификацию, но доступ к защищенному ресурсу требуется.
+Этот интерфейс определяет метод commence, который вызывается, когда происходит подобная ситуация.
 
-                // Конфигурация разрешений для HTTP-запросов.
-                .authorizeHttpRequests(authorize -> authorize
-                        // Разрешаем доступ к URL, содержащим "/public/**", всем пользователям.
-                        .requestMatchers("/public/**").permitAll()
-                        // Запрещаем остальные HTTP-запросы, требуем аутентификации.
-                        .anyRequest().authenticated()
+public interface AuthenticationEntryPoint {
+    void commence(HttpServletRequest request, HttpServletResponse response,
+                  AuthenticationException authException) throws IOException, ServletException;
+}
+commence: Метод вызывается, когда пользователь пытается получить доступ к защищенному ресурсу без аутентификации.
+Этот метод принимает HttpServletRequest, HttpServletResponse и AuthenticationException.
+Разработчик может использовать эти параметры для выполнения необходимых действий,
+таких как перенаправление пользователя на страницу входа или отправка пользователю сообщения об ошибке.
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // Создаем объект BasicAuthenticationEntryPoint для обработки базовой аутентификации
+        BasicAuthenticationEntryPoint authenticationEntryPoint = new BasicAuthenticationEntryPoint();
+
+        // Устанавливаем имя области (Realm) для объекта BasicAuthenticationEntryPoint
+        authenticationEntryPoint.setRealmName("Realm");
+
+        // Настраиваем HTTP Security
+        return httpSecurity
+                // Включаем базовую аутентификацию
+                .httpBasic(httpSecurityHttpBasicConfigurer ->
+                        httpSecurityHttpBasicConfigurer.authenticationEntryPoint(authenticationEntryPoint)
                 )
 
-                // Конфигурация формы входа.
-                .formLogin(Customizer.withDefaults())
+                // Устанавливаем правило для авторизации всех HTTP-запросов (все должны быть аутентифицированы)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.anyRequest().authenticated()
+                )
 
-                // Отключение CSRF-защиты для упрощения. Используйте осторожно в реальных приложениях.
-                .csrf(AbstractHttpConfigurer::disable)
+                // Настраиваем обработку исключений, в частности, указываем точку входа в систему
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
+                        httpSecurityExceptionHandlingConfigurer.authenticationEntryPoint(authenticationEntryPoint)
+                )
 
-                // Конфигурация обработки исключений в безопасности.
-                .exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer
-                        // Конфигурация точки входа в систему при отсутствии аутентификации.
-                        .authenticationEntryPoint((request, response, authException) ->
-                                response.sendRedirect("http://localhost:8080/public/sign-in.html")
-                        )
-                );
-
-        // Возврат экземпляра SecurityFilterChain, построенного с использованием настроек http.
-        return http.build();
+                // Собираем и возвращаем SecurityFilterChain
+                .build();
     }
+
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                // Конфигурация базовой аутентификации. В данном случае, закомментирована, возможно, не используется.
+//                //.httpBasic(Customizer.withDefaults())
+//
+//                // Конфигурация разрешений для HTTP-запросов.
+//                .authorizeHttpRequests(authorize -> authorize
+//                        // Разрешаем доступ к URL, содержащим "/public/**", всем пользователям.
+//                        .requestMatchers("/public/**").permitAll()
+//                        // Запрещаем остальные HTTP-запросы, требуем аутентификации.
+//                        .anyRequest().authenticated()
+//                )
+//
+//                // Конфигурация формы входа.
+//                .formLogin(Customizer.withDefaults())
+//
+//                // Отключение CSRF-защиты для упрощения. Используйте осторожно в реальных приложениях.
+//                .csrf(AbstractHttpConfigurer::disable)
+//
+//                // Конфигурация обработки исключений в безопасности.
+//                .exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer
+//                        // Конфигурация точки входа в систему при отсутствии аутентификации.
+//                        .authenticationEntryPoint((request, response, authException) ->
+//                                response.sendRedirect("http://localhost:8080/public/sign-in.html")
+//                        )
+//                );
+//
+//        // Возврат экземпляра SecurityFilterChain, построенного с использованием настроек http.
+//        return http.build();
+//    }
 
     @Bean
     public RouterFunction<ServerResponse> routerFunction() {
