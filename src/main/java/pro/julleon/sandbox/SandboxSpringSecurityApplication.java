53030751eb8928b736c83832180f496f8897d1e6
Julleon <julleonchik@gmail.com>
2024-02-23 22:41:24 +0900

    Настроена базовая конфигурация Spring Security в методе filterChain. - Установлены разрешения для HTTP-запросов: доступ к URL, содержащим "/public/**", разрешен всем пользователям, остальные запросы требуют аутентификации. - Настроена форма входа по умолчанию. - Отключена CSRF-защита для упрощения конфигурации. - Настроена обработка исключений с использованием `authenticationEntryPoint`: если аутентификация отсутствует, происходит перенаправление на страницу входа (в данном случае, "http://localhost:8080/public/sign-in.html").

--- src/main/java/pro/julleon/sandbox/SandboxSpringSecurityApplication.java
+++ src/main/java/pro/julleon/sandbox/SandboxSpringSecurityApplication.java
@@ -6,15 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.function.*;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
 
-import java.security.Principal;
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
 
 @SpringBootApplication
 public class SandboxSpringSecurityApplication {
@@ -24,15 +24,37 @@ public class SandboxSpringSecurityApplication {
     }
 
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .httpBasic(Customizer.withDefaults())
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().authenticated()
-//                );
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // Конфигурация базовой аутентификации. В данном случае, закомментирована, возможно, не используется.
+                //.httpBasic(Customizer.withDefaults())
+
+                // Конфигурация разрешений для HTTP-запросов.
+                .authorizeHttpRequests(authorize -> authorize
+                        // Разрешаем доступ к URL, содержащим "/public/**", всем пользователям.
+                        .requestMatchers("/public/**").permitAll()
+                        // Запрещаем остальные HTTP-запросы, требуем аутентификации.
+                        .anyRequest().authenticated()
+                )
+
+                // Конфигурация формы входа.
+                .formLogin(Customizer.withDefaults())
+
+                // Отключение CSRF-защиты для упрощения. Используйте осторожно в реальных приложениях.
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // Конфигурация обработки исключений в безопасности.
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer
+                        // Конфигурация точки входа в систему при отсутствии аутентификации.
+                        .authenticationEntryPoint((request, response, authException) ->
+                                response.sendRedirect("http://localhost:8080/public/sign-in.html")
+                        )
+                );
+
+        // Возврат экземпляра SecurityFilterChain, построенного с использованием настроек http.
+        return http.build();
+    }
 
     @Bean
     public RouterFunction<ServerResponse> routerFunction() {
